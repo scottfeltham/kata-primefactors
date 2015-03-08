a1400f86b33f86feee7866423708f0f3ad281ee0
Scott Feltham <scott.feltham@digital.hmrc.gov.uk>
2015-03-08 18:04:13 +0000

    initial commit

---
+++ src/main/scala/com/example/PrimeFactors.scala
@@ -0,0 +1,15 @@
+package com.example
+
+object PrimeFactors {
+  def result(real_number: Int): List[Int] = {
+    val list = List()
+    list
+  }
+
+//  def result(number: Int, list: List[Int] = List()): List[Int] = {
+//    for(n <- 2 to number if (number % n == 0)) {
+//      return result(number / n, list :+ n)
+//    }
+//    list
+//  }
+}
